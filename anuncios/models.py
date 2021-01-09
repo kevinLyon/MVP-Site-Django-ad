50a5e294539ee36a90485da3b73b811ecd16ef59
kevin Lyon <blackmaskdeveloper@gmail.com>
2021-01-09 11:38:01 -0300

    Criado model Anuncio/Corrigido erro em banco de dados

--- anuncios/models.py
+++ anuncios/models.py
@@ -7,5 +7,13 @@ class Categoria(models.Model):
         return self.titulo
 
 
-class Anuncio:
-    pass
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=40)
+    descricao = models.TextField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=11, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    modificado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
